83201e1ef602efe99cfeb6d67a9611fe435892c9
Mazhar <shaikmazhar54@gmail.com>
2021-05-27 16:53:56 +0530

    Added Json Parsing and Used gitignore

--- YouTubeone/Constants.swift
+++ YouTubeone/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Constants {
     
-    static var API_KEY = ""
+    static var API_KEY = "AIzaSyBRqAHqN2eZYL7LsoGTNwWyAZ0JUiaTV4M"
     static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
     static var API_URL =   "https://youtube.googleapis.com/youtube/v3/playlistItems?part=Snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
